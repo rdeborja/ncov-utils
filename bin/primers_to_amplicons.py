94e96d98a8fa4a803d29b6023369207ccbbd62b8
Richard de Borja <richard.deborja@gmail.com>
2020-06-30 14:26:22 -0400

    clean up code

--- bin/primers_to_amplicons.py
+++ bin/primers_to_amplicons.py
@@ -10,8 +10,10 @@ import ncov.utils.primers as utils
 parser = argparse.ArgumentParser(description='Create unique amplicon BED file')
 parser.add_argument('-p', '--primers', help='Primer scheme in BED format')
 parser.add_argument('--offset', default=30, help='Primer offset for coordinates')
-parser.add_argument('-o', '--output', default='out.bed', help='filename to write BED to')
-parser.add_argument('--pattern', default='nCoV-2019_', help='amplicon name pattern')
+parser.add_argument('-o', '--output', default='out.bed',
+                    help='filename to write BED to')
+parser.add_argument('--pattern', default='nCoV-2019_',
+                    help='amplicon name pattern')
 if len(sys.argv) <= 1:
     parser.print_help(sys.stderr)
     sys.exit(1)
@@ -19,10 +21,12 @@ args = parser.parse_args()
 
 primers = utils.import_bed_file(bed=args.primers)
 primer_pairs = utils.create_primer_pairs(primers=primers)
-amplicons = utils.create_amplicon_range(primer_pairs=primer_pairs, pattern=args.pattern)
-unique_amplicons = utils.create_unique_amplicons(amplicons, offset=args.offset)
+amplicons = utils.create_amplicon_range(primer_pairs=primer_pairs,
+                                        pattern=args.pattern)
+unique_amplicons = utils.create_unique_amplicons(amplicons=amplicons,
+                                                 offset=args.offset)
 with open(args.output, 'w') as file_o:
     for line in unique_amplicons:
-        file_o.write('\t'.join([line]))
+        file_o.write('\t'.join(line))
         file_o.write('\n')
 file_o.close()
